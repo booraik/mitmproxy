3f4f4c985feac32eb3f23503b9ce4a8f89e3d656
Nick Stepa <hired777@gmail.com>
2015-08-03 17:00:15 +0300

    New script

--- nfqueue.py
+++ nfqueue.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 import os
+import shlex, subprocess
 from scapy.all import *
 from netfilterqueue import NetfilterQueue
 
 ip_set = set()
-my_ip = "172.27.2.1"
-iptables1 = "iptables -t nat -A PREROUTING -p tcp -d {0} -j DNAT --to " + my_ip + ":2222"
-iptables2 = "iptables -t nat -A OUTPUT -p tcp -d " + my_ip + " --dport 2223 -j DNAT --to {0}:22"
+iptables = "iptables -t nat -A OUTPUT -p tcp --dport 2223 -j DNAT --to {0}:22"
 
 def process(pkt):
     global ip_set
@@ -16,14 +15,19 @@ def process(pkt):
         return
 
     pkt.set_mark(1)    
-    os.system(iptables1.format(ip.dst))
-    os.system(iptables2.format(ip.dst))
+    os.system(iptables.format(ip.dst))
     ip_set.add(ip.dst)
     pkt.repeat()
     
 nfqueue = NetfilterQueue()
 nfqueue.bind(0, process)
+os.system("iptables -t nat -A OUTPUT -p tcp --dport 22 -m mark --mark 0 -j NFQUEUE")
+os.system("iptables -t nat -A OUTPUT -p tcp --dport 22 -j DNAT --to 127.0.0.1:2222")
+proxy = subprocess.Popen(shlex.split("./mitmproxy_ssh -H 192.0.2.0 -P 2223")) #use 192.0.2.0 as IP for example
+
 try:
     nfqueue.run()
 except KeyboardInterrupt:
-    print
+    proxy.terminate()
+    os.system("iptables -t nat -F")
+    os.system("iptables -t nat -X")
